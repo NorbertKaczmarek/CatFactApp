be8fcc5f8c9736b595b3c7d6378983e1cc632427
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-05-22 16:35:05 +0200

    Add project files.

---
+++ CatFactService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Json;
+
+namespace CatFactApp
+{
+    internal interface ICatFactService
+    {
+        Task<CatFact> GetCatFactAsync(string url);
+        Task SaveCatFactToFileAsync(string path, CatFact catFact);
+    }
+
+    internal class CatFactService : ICatFactService
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+
+        public async Task<CatFact> GetCatFactAsync(string url)
+        {
+            var response = await httpClient.GetAsync(url);
+
+            if (response is null || response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                await Console.Out.WriteLineAsync("CatFact not found.");
+                return null;
+            }
+
+            var catFact = await response.Content.ReadFromJsonAsync<CatFact>();
+
+            if (catFact is null)
+            {
+                await Console.Out.WriteLineAsync("CatFact could not be mapped");
+                return null;
+            }
+
+            return catFact;
+        }
+
+        public async Task SaveCatFactToFileAsync(string path, CatFact catFact)
+        {
+            if (catFact is null) return;
+
+            using (var sw = new StreamWriter(path, true))
+            {
+                await sw.WriteLineAsync(catFact.Fact);
+            }
+
+            await Console.Out.WriteLineAsync($"CatFact saved to file: {path}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CatFactApp
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddSingleton<ICatFactService, CatFactService>()
+                .BuildServiceProvider();
+            
+            var catFactService = serviceCollection.GetService<ICatFactService>();
+
+            string url = "https://catfact.ninja/fact";
+            string fileName = "CatFacts.txt";
+
+            if (catFactService != null)
+            {
+
+                var catFact = await catFactService.GetCatFactAsync(url);
+                await catFactService.SaveCatFactToFileAsync(fileName, catFact);
+            }
+        }
+    }
+}
